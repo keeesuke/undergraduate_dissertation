c8d787e7fce95805d6ebd1622417bca9892bf6c5
keisuke1019 <keisukeo1019@gmail.com>
2019-01-28 13:02:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This repository is for my undergraduate reserch about "Extraction of factors describing the stability of retained austenite in low alloy TRIP steel"
+
---
+++ const.py
@@ -0,0 +1,10 @@
+sample1 = '1st-sample'
+sample2 = '2nd-sample'
+sample3 = '3rd-sample'
+#μm
+size_min = 0.3
+size_max = 110
+HORIZONTAL_SPLIT = 12
+VERTICAL_SPLIT = 12
+margin = 5
+test = 12
